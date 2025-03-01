3ade1c11612f3be326414dc55d2b122d5e66a690
Thamires Ouverney <touverneydarci@gmail.com>
2025-03-01 20:33:26 -0300

    Aula05

---
+++ CursoEmVideo_Exercicios/Scripts/script01.py
@@ -0,0 +1,10 @@
+Python 3.13.2 (tags/v3.13.2:4f8bb39, Feb  4 2025, 15:23:48) [MSC v.1942 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> nome = input ('Qual o seu nome?')
+Qual o seu nome? Thata
+>>> idade = input ('Quantos anos você tem?')
+Quantos anos você tem? 26
+>>> peso = input ('Qual o seu peso?')
+Qual o seu peso? 55,0
+>>> print('O seu nome é,', nome, ', você tem', idade, 'anos e pesa', peso, 'kg!')
+O seu nome é,  Thata , você tem  26 anos e pesa  55,0 kg!
---
+++ CursoEmVideo_Exercicios/Scripts/teste01.py
@@ -0,0 +1,4 @@
+nome = input ('Qual o seu nome?')
+idade = input ('Quantos anos você tem?')
+peso = input ('Qual o seu peso?')
+print('O seu nome é,', nome, ', você tem', idade, 'anos e pesa', peso, 'kg!')
